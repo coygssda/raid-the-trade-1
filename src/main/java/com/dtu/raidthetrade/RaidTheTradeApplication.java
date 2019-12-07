b7e066570fa2e241f8e84ab617d62de3873f0744
Aashish-Anand <aashishanand1997@gmail.com>
2019-12-07 15:49:35 +0530

    first commit

---
+++ src/main/java/com/dtu/raidthetrade/RaidTheTradeApplication.java
@@ -0,0 +1,13 @@
+package com.dtu.raidthetrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaidTheTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaidTheTradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dtu/raidthetrade/RaidTheTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtu.raidthetrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaidTheTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
